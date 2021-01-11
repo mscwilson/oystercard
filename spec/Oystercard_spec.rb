0cbd8315356686a8e10a09a2af23991245d6d5f4
Miranda Wilson <mirandascwilson@gmail.com>
2021-01-11 15:27:36 +0000

    added test and code for initializing balance of 0

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,3 +1,13 @@
+require "oystercard"
+
 describe Oystercard do
+
+  describe "#balance" do
+
+    it "has balance of 0 on initializing" do
+      expect(subject.balance).to eq 0
+    end
+
+  end
   
 end
